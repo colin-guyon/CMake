fc77ca880e604a59e91fbc62a4e74614bbcd2959
comicfans44 <comicfans44@gmail.com>
2016-12-26 13:49:24 -0500

    add fastbuild flag to cmState

--- Source/cmState.h
+++ Source/cmState.h
@@ -146,6 +146,8 @@ public:
   bool UseNMake() const;
   void SetMSYSShell(bool mSYSShell);
   bool UseMSYSShell() const;
+  void SetFastbuildMake(bool fastbuildMake);
+  bool UseFastbuildMake() const;
 
   unsigned int GetCacheMajorVersion() const;
   unsigned int GetCacheMinorVersion() const;
@@ -180,6 +182,7 @@ private:
   bool MinGWMake;
   bool NMake;
   bool MSYSShell;
+  bool FastbuildMake;
 };
 
 #endif
